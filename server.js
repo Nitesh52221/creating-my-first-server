2fe59b093d43e2f010085cb0e080ab892c1aed25
Nitesh <niteshtiwari5222@gmail.com>
2022-08-22 20:03:41 +0530

    Adding Routing

--- server.js
+++ server.js
@@ -1,12 +1,40 @@
 // http://localhost:8081
 const http = require('http');
 
+const toDoList = ["Complete Node Byte", "play Criket"];
+
 const port = 8081;
 
-http.createServer((Request, Response) => {
-    Response.writeHead(200, { 'Content-Type': 'text/html' });
-    Response.write('<h1>Helllo , this is from my server</h1>');
-    Response.end();
+http.createServer((req, res) => {
+    const { method, url } = req
+    if (url === "/todos") {
+        if (method === "GET") {
+            res.writeHead(200, { "Content-type": "text/hmtl" })
+            res.write(toDoList.toString())
+            res.end();
+        } else if (method === "POST") {
+            let body = "";
+            req.on("error", (err) => {
+                console.error(err);
+
+            }).on("data", (chunk) => {
+                body += chunk;
+                console.log(chunk)
+            }).on("end", () => {
+                body = JSON.parse(body);
+                console.log("data : ", body)
+            })
+
+        } else {
+            res.writeHead(501)
+        }
+
+    }
+    else {
+        res.writeHead(404)
+
+    }
+
 }).listen(port, () => {
     console.log(`My first Node Js server started on port ${port}`);
 });
