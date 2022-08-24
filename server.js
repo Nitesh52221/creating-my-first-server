3bbf67d0ca624f03e2664d577e5ceda8b99558ff
Nitesh <niteshtiwari5222@gmail.com>
2022-08-24 16:05:57 +0530

    Added new Routes

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 // http://localhost:8081
 const http = require('http');
 
+
 const toDoList = ["Complete Node Byte", "play Criket"];
 
 const port = 8081;
 
 http.createServer((req, res) => {
-    const { method, url } = req
+
+    const { method, url } = req;
+
     if (url === "/todos") {
+
         if (method === "GET") {
             res.writeHead(200, { "Content-type": "text/hmtl" })
             res.write(toDoList.toString())
@@ -19,13 +23,40 @@ http.createServer((req, res) => {
 
             }).on("data", (chunk) => {
                 body += chunk;
-                console.log(chunk)
+
             }).on("end", () => {
                 body = JSON.parse(body);
-                console.log("data : ", body)
+                let newToDo = toDoList;
+                newToDo.push(body.item);
+
+                res.writeHead(201);
+
+
             })
 
-        } else {
+        }
+        else if (method === "DELETE") {
+            let body = '';
+            req.on('error', () => {
+                console.error(err);
+            }).on('data', (chunk) => {
+                body += chunk;
+            }).on('end', () => {
+                body = JSON.parse(body);
+                let deleteThis = body.item;
+
+                for (let i = 0; i < toDoList.length; i++) {
+                    if (toDoList[i] === deleteThis) {
+                        toDoList.splice(i, 1);
+                        break;
+
+                    }
+                }
+                console.log("To Do list", toDoList);
+                res.writeHead(204);
+            })
+        }
+        else {
             res.writeHead(501)
         }
 
